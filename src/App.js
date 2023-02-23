779581f7dac4760695918d9ef9a29eb2ca34f627
ab-rai <ab.rai32768@gmail.com>
2023-02-23 16:40:20 +0530

    Adding logic and design for the game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from 'react';
+import './App.css'
+import Box from './Components/Box';
 function App() {
+  const [boxInputs,setBoxInputs] = useState([-1,-1,-1,-1,-1,-1,-1,-1,-1]);
+  const [turn, setTurn] = useState("0");
+  const [winner, setWinner] = useState(null);
+  // const [boxInputs,setBoxInputs] = useState([0,1,2,3,4,5,6,7,8]) 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      
+      <div className="header">
+        <h2>Tic Tac Toe</h2>
+      </div>
+
+      <div className="body">
+        {
+          boxInputs.map((el,index)=>{
+            return <Box key={index} boxInputs={boxInputs} setBoxInputs={setBoxInputs} index = {index} 
+                        turn = {turn} setTurn = {setTurn} winner = {winner} setWinner = {setWinner}/>
+          })
+        }  
+      </div>
+      
+      <div className="footer">
+        <h2>Developed by <b>Ab Rai</b></h2>
+        <h2>  Winner is {winner !== null ? winner : "Not Decided Yet" } </h2>
+      </div>
+    
     </div>
+      
   );
 }
 
---
+++ src/Components/Box.js
@@ -0,0 +1,87 @@
+let Box = (props) => {
+    let {boxInputs, setBoxInputs, index, turn, setTurn, winner, setWinner} = props
+    function handleButtonClick(index){
+        if(boxInputs[index] !== -1){
+            return;
+        }
+        else{
+            let newBoxInputs = boxInputs;
+            if(turn === "0"){
+                newBoxInputs[index] = "0";
+                setTurn("X");
+            }
+            else{
+                newBoxInputs[index] ="X";
+                setTurn("0");
+            }
+            setBoxInputs(newBoxInputs)
+        }
+        checkWinner();
+    }
+    function checkWinner(){
+        // for(let i=0;i<3;i++){
+        //     for(let j=0;j<3;j++){
+
+        //     }
+        // }
+        console.log("Checking winner", turn)
+        console.log(boxInputs[0],boxInputs[1],boxInputs[2],boxInputs[3],boxInputs[4],boxInputs[5],boxInputs[6],boxInputs[7],boxInputs[8])
+        if(boxInputs[0] !== -1){
+            if( (boxInputs[0] === boxInputs[1] && boxInputs[1] === boxInputs[2]) ||
+                (boxInputs[0] === boxInputs[3] && boxInputs[3] === boxInputs[6]) ||
+                (boxInputs[0] === boxInputs[4] && boxInputs[4] === boxInputs[8]) 
+                ){
+                console.log("Can in true")
+                handleSetWinner()
+              }
+        }
+        if(boxInputs[1] !== -1){
+            if( boxInputs[1] === boxInputs[4] && boxInputs[4] === boxInputs[7]) {
+                console.log("Can in 2 true")
+                handleSetWinner()
+              }
+        }
+       if(boxInputs[2] !== -1){
+            if( (boxInputs[2] === boxInputs[5] && boxInputs[5] === boxInputs[8]) ||
+                (boxInputs[2] === boxInputs[4] && boxInputs[4] === boxInputs[6])
+                ) {
+                console.log("Can in 3 true")
+                handleSetWinner()
+              }
+        }
+        if(boxInputs[6] !== -1){
+            if( boxInputs[6] === boxInputs[7] && boxInputs[7] === boxInputs[8]) {
+                console.log("Can in 4 true")
+                handleSetWinner()
+              }
+        } 
+        if(boxInputs[3] !== -1){
+            if( boxInputs[3] === boxInputs[4] && boxInputs[4] === boxInputs[5]) {
+                console.log("Can in 5 true")
+                handleSetWinner()
+              }
+        }
+
+
+    }
+
+    function handleSetWinner(){
+        if(turn === "0"){
+            setWinner("0")
+        }
+        else{
+            setWinner("X")
+        }
+    }
+    return (
+        <div className="box">
+            <button disabled={winner !== null} onClick={()=>handleButtonClick(index)}>{boxInputs[index] === -1 ? "" : boxInputs[index]}</button>
+        </div>
+    );
+
+    
+}
+
+
+
+export default Box;
